71ea6286af7f7b1cec29ff6dd36863b84afbf82d
Marcelo Bastos <xxatkinsxx@gmail.com>
2025-01-18 00:43:06 +0100

    wip: dash navigation

--- src/components/dashboard-navigation.tsx
+++ src/components/dashboard-navigation.tsx
@@ -1,47 +1,66 @@
 "use client";
 
-import { FileText, Truck, ClipboardList, User } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { LogoImg } from "@/assets";
+import Image from "next/image";
+import {
+  // FileText,
+  Truck,
+  ClipboardList,
+  User,
+  Home,
+  LogOut,
+} from "lucide-react";
+import { useRouter, usePathname } from "next/navigation";
+import { MenuItem } from "@/components";
 
 const DashboardNavigation = () => {
   const router = useRouter();
+  const currentPath = usePathname();
+
+  const routes = [
+    { path: "/dashboard", title: "Home", icon: Home },
+    { path: "/entregas", title: "Entregas", icon: Truck },
+    { path: "/gestao", title: "Gestão", icon: ClipboardList },
+    { path: "/conta", title: "Conta", icon: User },
+  ];
+
+  const isActive = (currentPath: string, path: string): string =>
+    currentPath === path ? "text-[#5932EA]" : "hover:text-violet-600";
 
   return (
-    <div className="col-span-1 w-full bg-white h-screen flex flex-col items-center py-6 space-y-8">
-      <div className="w-12 h-12">
-        <div className="bg-violet-600 w-full h-full rounded-lg flex items-center justify-center">
-          <div className="w-6 h-6 bg-white rounded" />
-        </div>
+    <div className="col-span-1 w-full h-screen flex flex-col items-center justify-betweens py-6 space-y-8">
+      <div>
+        <Image src={LogoImg} alt="NzolaTrack logo" />
       </div>
 
-      <nav className="flex flex-col space-y-6">
-        <button
-          onClick={() => router.push("/deliveries")}
-          className="p-3 text-violet-600 bg-violet-50 rounded-lg"
+      <nav className="flex flex-col space-y-6 w-full items-center">
+        {routes.map((route) => (
+          <MenuItem
+            key={route.path}
+            onClick={() => router.push(route.path)}
+            className={`${isActive(currentPath, route.path)}`}
+            icon={route.icon}
+            title={route.title}
+          />
+        ))}
+        {/* <button
+          onClick={() => router.push("/relatorios")}
+          className={`flex flex-col items-center justify-center p-3 rounded-lg ${isActive(
+            "/relatorios"
+          )}`}
         >
-          <Truck size={24} />
-        </button>
-        <button
-          onClick={() => router.push("/trucks")}
-          className="p-3 text-gray-400 hover:text-violet-600 hover:bg-violet-50 rounded-lg"
-        >
-          <ClipboardList size={24} />
-        </button>
-        <button className="p-3 text-gray-400 hover:text-violet-600 hover:bg-violet-50 rounded-lg">
-          <FileText size={24} />
-        </button>
-        {/* <button className="p-3 text-gray-400 hover:text-violet-600 hover:bg-violet-50 rounded-lg">
-          <Settings size={24} />
+          <FileText />
+          <span className="text-md mt-2">Relatórios</span>
         </button> */}
       </nav>
 
       <div className="mt-auto">
-        <button
-          onClick={() => router.push("/account")}
-          className="p-3 text-gray-400 hover:text-violet-600 hover:bg-violet-50 rounded-lg"
-        >
-          <User size={24} />
-        </button>
+        <MenuItem
+          onClick={() => router.push("/login")}
+          className={`${isActive(currentPath, "/login")}`}
+          icon={LogOut}
+          title="Sair"
+        />
       </div>
     </div>
   );
